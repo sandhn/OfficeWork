f8cdc9862f051117671f06906f774b50e09f75cc
sandhn <sandhn@DIN35004883.corp.capgemini.com>
2018-06-05 09:52:26 +0530

    Done Prog2- Hello World

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,4 +1,10 @@
 
-public class Hello_World {
-
+public class Hello_World 
+{
+	public static void main(String[] args) 
+    {
+              
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
 }
