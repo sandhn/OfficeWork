e1fb69335c7ca45968a14301a50e9c333a2e55df
sandhn <sandhn@DIN35004883.corp.capgemini.com>
2018-07-13 14:27:21 +0530

    Removing spaces in a string

---
+++ src/Strings/RemovingSpaces.java
@@ -0,0 +1,31 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemovingSpaces {
+
+	public static void main(String[] args)
+	{
+		String str= "I Love My India";
+			
+		//System.out.println(str.replace(" ", ""));  ---> Using replace function
+		
+		 char[] s = str.toCharArray();
+		 
+	        StringBuffer sb = new StringBuffer();
+	 
+	        for (int i = 0; i < s.length; i++)
+	        {
+	            if( (s[i] != ' ') )
+	            {
+	                sb.append(s[i]);
+	            }
+	        }
+	
+	        System.out.println(sb); 
+		 
+	
+	}
+
+}
